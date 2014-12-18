5c8ff24d2cd66220af2e597f12e295bd8c6a8da2
LAK132 <lakridesagain@gmail.com>
2014-12-18 18:27:40 +1030

    Work

--- RecordMyDrive/App.cs
+++ RecordMyDrive/App.cs
@@ -4,18 +4,51 @@ using Xamarin.Forms;
 namespace RecordMyDrive
 {
 	public class App
-	{
-		public static Page GetMainPage ()
-		{	
-			return new ContentPage 
-			{ 
-				Content = new Label 
-				{
-					Text = "Hello, Forms!",
-					VerticalOptions = LayoutOptions.CenterAndExpand,
-					HorizontalOptions = LayoutOptions.CenterAndExpand,
+	{	
+		static ContentPage mainPage = new ContentPage {
+			Title = "Record My Drive",
+			/*Content = new Label {
+				Text = "Hello, Forms!",
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand
+			}*/
+			Content = new StackLayout {
+				Spacing = 20,
+				Padding = 50,
+				VerticalOptions = LayoutOptions.Center,
+				Children = {
+					new Label { Text = "Disclaimer:" },
+					new Button { 
+						Text = "Begin",
+						BackgroundColor = Color.FromRgb(1, 100, 1)
+					}
 				}
-			};
+			}
+		};
+
+		static ContentPage profilePage = new ContentPage {
+			Title = "Settings",
+			Icon = "Icon.png",
+			Content = new StackLayout {
+				Spacing = 20, 
+				Padding = 50,
+				VerticalOptions = LayoutOptions.Center,
+				Children = {
+					new Entry { Placeholder = "Username" },
+					new Entry { Placeholder = "Password", IsPassword = true },
+					new Button {
+						Text = "Login",
+						TextColor = Color.White,
+						BackgroundColor = Color.FromHex("77D065") 
+					}
+				}
+			}
+		};
+
+
+		public static Page GetMainPage ()
+		{
+			return new TabbedPage { Children = { mainPage, profilePage } };
 		}
 	}
 }
